fb68641120fc59b1d1575db585a4c717dca3980c
Marten Seemann <martenseemann@gmail.com>
2020-01-14 18:05:23 +0700

    collapse packetHandlerManager.Add and AddIfNotTaken into one function

--- server.go
+++ server.go
@@ -33,11 +33,10 @@ type unknownPacketHandler interface {
 }
 
 type packetHandlerManager interface {
+	sessionRunner
 	io.Closer
 	SetServer(unknownPacketHandler)
 	CloseServer()
-	sessionRunner
-	AddIfNotTaken(protocol.ConnectionID, packetHandler) bool
 }
 
 type quicSession interface {
@@ -478,10 +477,9 @@ func (s *baseServer) createNewSession(
 		s.logger,
 		version,
 	)
-	added := s.sessionHandler.AddIfNotTaken(clientDestConnID, sess)
-	// We're already keeping track of this connection ID.
-	// This might happen if we receive two copies of the Initial at the same time.
-	if !added {
+	if added := s.sessionHandler.Add(clientDestConnID, sess); !added {
+		// We're already keeping track of this connection ID.
+		// This might happen if we receive two copies of the Initial at the same time.
 		return nil
 	}
 	s.sessionHandler.Add(srcConnID, sess)
